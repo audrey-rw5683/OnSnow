7be0ec36152558d1fd8fe980d08522fabd5613c7
Audrey <audrey.rw5683@gmail.com>
2024-09-22 18:38:56 -0700

    fix misaka installation

--- OnSnow/groups/models.py
+++ OnSnow/groups/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.utils.text import slugify
 # Create your models here.
+
+import misaka 
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+from django import template
+register = template.Library()
+
+class Group(models.Model):
+    pass
+
+class GroupMember(models.Model):
+    group = models.ForeignKey(Group, related_name='memberships')
